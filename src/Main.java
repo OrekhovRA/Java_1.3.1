31276a84021a337d242d93f8affb90a6323173c1
Orekhov Roman <orekhov-roman44@yandex.ru>
2022-01-15 20:04:51 +0300

    commit 1

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int price = 5951;
+        int mile = 20;
+        int total = price / mile;
+        System.out.println(total);
+    }
+}
+
