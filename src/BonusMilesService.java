97891927ba7d7593bbfd241ed08744df209277a5
Orekhov Roman <orekhov-roman44@yandex.ru>
2022-02-07 16:23:34 +0300

    commit 2

---
+++ src/BonusMilesService.java
@@ -0,0 +1,9 @@
+public class BonusMilesService {
+    public int calculate(int cost) {
+        int mile = 20;
+        int total = cost / mile;
+        return total;
+    }
+
+
+}
--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        int price = 5951;
-        int mile = 20;
-        int total = price / mile;
-        System.out.println(total);
+        BonusMilesService service = new BonusMilesService();
+        int price = 10_000;
+        int miles = service.calculate(price);
+        System.out.println(miles);
+
+//        public class Main {
+//            public static void main(String[] args) {
+//                int price = 3298;
+//                int mile = 20;
+//                int total = price / mile;
+//                System.out.println(total);
+//            }
+//        }
+
     }
 }
-
